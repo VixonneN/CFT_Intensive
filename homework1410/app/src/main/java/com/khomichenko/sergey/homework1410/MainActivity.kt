e5e495f62526275b24b89c36a92fb1d8660f7de6
Хомиченко Сергей Геннадьевич <sergey.khomichenko@yandex.ru>
2021-10-15 03:36:12 -0700

    initial commit

---
+++ homework1410/app/src/main/java/com/khomichenko/sergey/homework1410/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.khomichenko.sergey.homework1410
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
